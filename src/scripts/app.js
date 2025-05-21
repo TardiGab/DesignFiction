656d8a29a0edeb952d57075848fe3573761e3d63
TardiGab <tardigab@gmail.com>
2025-05-21 16:59:43 +0200

    Added customerspace form
    Still need to fix the select input

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,32 @@
-console.info('Hello world');
+"use strict";
+
+const loginInputs = document.querySelectorAll(".login__input");
+const contactInputs = document.querySelectorAll(".contact__input");
+
+loginInputs.forEach(input => {
+  const label = input.previousElementSibling; 
+
+  input.addEventListener("focus", function () {
+    label.classList.add("login__label--active");  
+  });
+
+  input.addEventListener("blur", function () {
+    if (input.value === "") {
+      label.classList.remove("login__label--active");
+    }
+  });
+});
+
+contactInputs.forEach(input => {
+  const label = input.previousElementSibling; 
+
+  input.addEventListener("focus", function () {
+    label.classList.add("contact__label--active");  
+  });
+
+  input.addEventListener("blur", function () {
+    if (input.value === "") {
+      label.classList.remove("contact__label--active");
+    }
+  });
+});
